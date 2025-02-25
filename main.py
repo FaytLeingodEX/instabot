d4994892df20855fcc78852210523de142c9137e
FaytLeingodEX <lehidale@gmail.com>
2025-02-25 23:46:38 +0100

    Added main.py and requirements.txt

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 print("Hello, Instagram-Bot läuft!")
+import instaloader
+import os
+from dotenv import load_dotenv
+
+# .env Datei laden
+load_dotenv()
+
+# Instagram-Login
+USERNAME = os.getenv("INSTAGRAM_USERNAME")
+PASSWORD = os.getenv("INSTAGRAM_PASSWORD")
+
+bot = instaloader.Instaloader()
+
+try:
+    bot.login(USERNAME, PASSWORD)
+    print("✅ Erfolgreich bei Instagram eingeloggt!")
+except Exception as e:
+    print(f"❌ Fehler beim Login: {e}")
